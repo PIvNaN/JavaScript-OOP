9cd1d9a1a162e678e123f23f1f205fcc6c9575fd
Iva Petkova <iva.g.petkova@abv.bg>
2017-05-08 05:20:34 +0300

    Create this.js

---
+++ Exercises/Functions/this.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const modifiers = [
+	function (x) { return x + 1; },
+	function (x) { return x * 1; },
+	function (x) { return 1; }
+];
+
+const modifiers2 = [
+	function (x) { return x + 2; },
+	function (x) { return x * 2; },
+	function (x) { return 2; }
+];
+
+const modifiers3 = [
+	function (x) { return x + 3; },
+	function (x) { return x * 3; },
+	function (x) { return 3; }
+];
+
+Array.prototype.someFunc = function () {
+	console.log(this[0].toString());
+};
+
+modifiers.someFunc();
+modifiers2.someFunc();
+modifiers3.someFunc();
