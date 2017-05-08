d999b9ea3e3fffee12006e348ffe902f77fb1585
Iva Petkova <iva.g.petkova@abv.bg>
2017-05-08 05:18:58 +0300

    Create functionsInArray.js

---
+++ Exercises/Functions/functionsInArray.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const modifies = [
+	 function (x) { return x + 7; },
+	 function (x) { return x * 2; },
+	 function (x) { return 42; }
+];
+
+function applyFunctionOver11(f) {
+	 return f(11);
+}
+
+modifies.map(applyFunctionOver11)
+	 .forEach(x => console.log(x));
+
+
+// Or:
+
+modifies.map(f => f(11))
+	 .forEach(x => console.log(x));
