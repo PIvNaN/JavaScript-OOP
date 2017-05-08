c30f7e324a6520883bea069fc14193d6dec1e35d
Iva Petkova <iva.g.petkova@abv.bg>
2017-05-08 05:23:00 +0300

    Create fibonacci.js

---
+++ Exercises/Functions/Recursion/fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+console.log(fibonacci(4));
+// 5
+
+console.log(fibonacci(5));
+// 8
